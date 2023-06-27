6e7ea951790dfb2a9dea33dc01d75b71a360de7b
smplfi <137914302+smplfi@users.noreply.github.com>
2023-06-27 10:39:16 -0600

    Update config.js

--- config.js
+++ config.js
@@ -11,16 +11,16 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	// name: 'John',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	// greetingMorning: 'Good morning!',
+	// greetingAfternoon: 'Good afternoon,',
+	// greetingEvening: 'Good evening,',
+	// greetingNight: 'Go to Sleep!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
